25c58f9de72b92205fab1f7688681640eccfa0c7
Ekou LOGOSU-TEKO <patricelogosuteko@gmail.com>
2022-09-16 18:42:17 +0000

    removed env files from gitignore (I know, not a good practice, but I wish to track them since its a learning project)

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=rootroot
+export POSTGRES_HOST=udagramp3instance.cycq1fgw8jit.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramp3
+export AWS_BUCKET=cloud-dev-project-3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
